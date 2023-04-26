f870520db6fc6d70ae563d0aadf031d49601c3d5
Pramoth Suwanpech <pramoth@geniustree.co.th>
2023-04-27 00:35:30 +0700

    apply d.dopa

--- services/src/main/java/org/keycloak/broker/oidc/OIDCIdentityProvider.java
+++ services/src/main/java/org/keycloak/broker/oidc/OIDCIdentityProvider.java
@@ -675,7 +675,7 @@ public class OIDCIdentityProvider extends AbstractOAuth2IdentityProvider<OIDCIde
     }
 
     public JsonWebToken validateToken(String encodedToken) {
-        boolean ignoreAudience = false;
+        boolean ignoreAudience = true;
 
         return validateToken(encodedToken, ignoreAudience);
     }
@@ -933,7 +933,8 @@ public class OIDCIdentityProvider extends AbstractOAuth2IdentityProvider<OIDCIde
         String nonce = (String) context.getContextData().get(BROKER_NONCE_PARAM);
 
         if (nonce == null) {
-            throw new IdentityBrokerException("OpenID Provider [" + getConfig().getProviderId() + "] did not return a nonce");
+            logger.info("no nonce for this OIDC");
+            return;
         }
 
         String expectedNonce = authenticationSession.getClientNote(BROKER_NONCE_PARAM);
